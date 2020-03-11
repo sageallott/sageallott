d089098fd06273924c8ddf41aebe1e002b638072
sageallott <47040054+sageallott@users.noreply.github.com>
2020-03-11 14:22:06 -0400

    Updates with active class function

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -49,6 +49,7 @@ function isActive() {
 */
 
 // build the nav
+
 function buildNavMenu() {
     for (let item of pageSections) {
         let section = document.createElement('li');
@@ -61,6 +62,29 @@ function buildNavMenu() {
 
 // Add class 'active' to section when near top of viewport
 
+function addActiveClass () {
+    window.addEventListener('scroll', function () {
+        let section = checkforActive();
+        section.classList.add('your-active-class');
+        // remove active class from other sections
+        for (let pageSect of pageSections) {
+            if (pageSect.id !== section.id & pageSect.classList.contains('your-active-class')) {
+                pageSect.classList.remove('your-active-class');
+            }
+        }
+        // add header style
+        const activeStyle = document.querySelector('li[data-nav="' + section.id + '"]');
+        activeStyle.classList.add('section_active_style');
+        
+        // remove header style from other sections
+        const headers = document.querySelectorAll('.menu__link');
+        for (let head of headers) {
+            if (head.dataset.nav !== activeStyle.dataset.nav & head.classList.contains('section_active_style')) {
+                head.classList.remove('section_active_style');
+            }
+        };
+    });
+}
 
 // Scroll to anchor ID using scrollTO event
 
